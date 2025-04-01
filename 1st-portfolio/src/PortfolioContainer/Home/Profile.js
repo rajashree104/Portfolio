8b3f6eeff5dd39f4e43263d1d24096bb23d52446
rajashree <srajashree104@gmail.com>
2025-04-01 16:20:17 +0530

    icons

---
+++ 1st-portfolio/src/PortfolioContainer/Home/Profile.js
@@ -0,0 +1,29 @@
+import React from 'react'
+
+export default function Profile() {
+    return (
+        <div className='profile-Container '>
+            <div className='profile-parent'>
+                <div className='profile-details'>
+                    <div className='colz'>
+                        <a href='#'>
+                            <i className='fa fa-facebook'></i>
+                        </a>
+                        <a href='#'>
+                            <i className='fa fa-google-plus'></i>
+                        </a>
+                        <a href='#'>
+                            <i className='fa fa-instagram'></i>
+                        </a>
+                        <a href='#'>
+                            <i className='fa fa-youtube'></i>
+                        </a>
+                        <a href='#'>
+                            <i className='fa fa-twitter'></i>
+                        </a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+}
